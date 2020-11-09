2efd6eab7796ccbd04ea58ae489053f84e418564
remy moulinier <rmoulinier@outlook.com>
2020-11-09 14:57:11 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Name
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @response = if @question == 'I am going to work'
+                  'Great!'
+                elsif @question.ends_with?('?')
+                  'Silly question, get dressed and go to work!'
+                else
+                  "I don't care, get dressed and go to work!"
+                end
+  end
+end
